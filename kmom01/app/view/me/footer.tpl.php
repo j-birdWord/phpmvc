5703f51c8314dddd91f19d48fabcbc264cdd5907
Jacob Svedman <jasv14@student.bth.se>
2015-02-16 17:24:21 +0100

    Added link to git

--- kmom01/app/view/me/footer.tpl.php
+++ kmom01/app/view/me/footer.tpl.php
@@ -1,2 +1,2 @@
-<footer><span class='sitefooter'>Copyright (c) Jacob Svedman (jasv14@student.bth.se) | <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a></span>
+<footer><span class='sitefooter'>Copyright (c) Jacob Svedman (jasv14@student.bth.se) | <a href='https://github.com/j-birdWord/phpmvc'>J-Bird-MVC på GitHub</a> | <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a></span>
 </footer>
